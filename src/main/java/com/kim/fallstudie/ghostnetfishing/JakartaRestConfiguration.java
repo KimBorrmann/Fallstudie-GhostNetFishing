47180e6f73e78f9f01364adf4a732bf5c98d9b8c
KimBorrmann <kim@haldehisle.de>
2024-10-20 17:51:43 +0200

    Initial commit

---
+++ src/main/java/com/kim/fallstudie/ghostnetfishing/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.kim.fallstudie.ghostnetfishing;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
